1b2de00a5c8d9fe91e30c7c5867eef5f2f6dbf38
Dawn inShadow <zpy18746442232@gmail.com>
2017-08-17 10:33:48 +0800

    要死了Orz

--- Scripts/index.js
+++ Scripts/index.js
@@ -7,6 +7,54 @@ $(document).ready(function(){
 
 
 
+
+
+
+
+    var win_h=document.body.clientHeight;
+    console.log(win_h);
+    var win_w=document.body.clientWidth;
+
+
+   var scroll_parallax=$('.scroll_parallax_gb');
+
+     addEvent(window,'scroll',onScroll);
+    onScroll();
+
+    function onScroll(e){
+        var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+        console.log(scrollTop);
+       scroll_parallax.css("background-position-y",400+scrollTop*.3);
+    }
+
+    function addEvent(eventTarget, eventType, eventHandler) {
+        if (eventTarget.addEventListener) {
+            eventTarget.addEventListener(eventType, eventHandler, false);
+        } else {
+            if (eventTarget.attachEvent) {
+                eventType = "on" + eventType;
+                eventTarget.attachEvent(eventType, eventHandler);
+            } else {
+                eventTarget["on" + eventType] = eventHandler;
+            }
+        }
+    }
+
+
+
+    $(window).scroll(function () {
+        if($(window).scrollTop()>0){
+            $('header').addClass("mini");
+        }else {
+            $('header').removeClass("mini");
+        }
+
+
+
+    })
+    $('.lunbo_z').css("height",win_h).css("width",win_w);
+    $('.main').css("top",win_h);
+
     $('nav >li').mouseenter(function(){
 
         $(this).children().removeClass("bouncein bounceout");
