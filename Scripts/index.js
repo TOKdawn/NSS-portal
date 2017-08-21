78ffab95382d39a1578224919e43ff815a176630
Dawn inShadow <zpy18746442232@gmail.com>
2017-08-21 16:05:23 +0800

    完成80%

--- Scripts/index.js
+++ Scripts/index.js
@@ -3,28 +3,67 @@
  */
 
 
+
 $(document).ready(function(){
 
-
-
-
+    var wow = new WOW({
+        boxClass: 'wow',
+        animateClass: 'animated',
+        offset: 0,
+        mobile: true,
+        live: true
+    });
+    wow.init();
 
 
 
     var win_h=document.body.clientHeight;
-    console.log(win_h);
+   // console.log(win_h);
     var win_w=document.body.clientWidth;
 
+   $('.lunbo_z').css("height",win_h).css("width",win_w);
+    $('.main').css("top",win_h);
+    $('.article').css("height",win_h-60).css("width",win_w);
+    //$('.lunbo_img').css("height",win_h).css("width",win_w);
 
-   var scroll_parallax=$('.scroll_parallax_gb');
+
+
+    $(window).resize(function () {          //当浏览器大小变化时
+        var win_h=document.body.clientHeight;
+       // console.log(win_h);
+        var win_w=document.body.clientWidth;
+        if(win_w<=1330){
+            console.log("aaaaa");
+            win_w=1330;
+        }
+        $('.lunbo_z').css("height",win_h).css("width",win_w);
+        $('.main').css("top",win_h);
+        $('.article').css("height",win_h-60).css("width",win_w);
+        $('.lunbo_img').css("height",win_h).css("width",win_w);
+
+    });
+
+
+
+    var scroll_parallax=$('.scroll_parallax_gb');
 
      addEvent(window,'scroll',onScroll);
     onScroll();
 
     function onScroll(e){
+
         var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
-        console.log(scrollTop);
-       scroll_parallax.css("background-position-y",400+scrollTop*.3);
+         if(scrollTop==0){
+             scrollTop=-1666;
+             //垃圾火狐!!!!!
+         }
+
+        //console.log(scrollTop);
+       //  var Orz="0px "+400+scrollTop*.3;
+       //  console.log(Orz);
+       // scroll_parallax.css("background-position",Orz);
+    //    垃圾火狐QAQ
+        scroll_parallax.css("background-position-y",400+scrollTop*.3);
     }
 
     function addEvent(eventTarget, eventType, eventHandler) {
@@ -52,28 +91,26 @@ $(document).ready(function(){
 
 
     })
-    $('.lunbo_z').css("height",win_h).css("width",win_w);
-    $('.main').css("top",win_h);
 
     $('nav >li').mouseenter(function(){
 
-        $(this).children().removeClass("bouncein bounceout");
-        $(this).children().addClass("bouncein");
+        $(this).children().removeClass("zbouncein zbounceout");
+        $(this).children().addClass("zbouncein");
     });
     $("nav >li").mouseleave(function(){
-        $(this).children().removeClass("bouncein bounceout");
-        $(this ).children().addClass("bounceout");
+        $(this).children().removeClass("zbouncein zbounceout");
+        $(this ).children().addClass("zbounceout");
     });
     $('#studio').mouseenter(function(){
 
-        $('#studio >a').removeClass("jackout jackin");
-        $('#studio >a').addClass("jackin");
+        $('#studio >a').removeClass("zjackout zjackin");
+        $('#studio >a').addClass("zjackin");
 
     });
     $('#studio').mouseleave(function(){
 
-        $('#studio >a').removeClass("jackout jackin");
-        $('#studio >a').addClass("jackout");
+        $('#studio >a').removeClass("zjackout zjackin");
+        $('#studio >a').addClass("zjackout");
 
     });
     $('#studio').mouseover(function(){
